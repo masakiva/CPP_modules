be61ad9f9ace3581318997f6a42a25dc70df047e
pusher <kumo.git@mailnull.com>
2021-10-24 16:05:50 +0200

    ex03

--- day_05/ex03/Intern.class.cpp
+++ day_05/ex03/Intern.class.cpp
@@ -6,7 +6,7 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 17:13:05 by mvidal-a          #+#    #+#             */
-/*   Updated: 2021/10/23 17:48:14 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2021/10/24 16:05:26 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,63 @@ Intern&		Intern::operator=( const Intern & rhs )
 	return ( *this );
 }
 
-Form*		Intern::makeForms( std::string formName, std::string formTarget )
+Form*		Intern::makeForm( std::string formName, std::string formTarget )
 {
-// tableau de pointeurs sur fonctions + tableau de strings, avec new
-	std::cout << "Intern creates " << std::endl;
+	Form*			(Intern::*formCalls[3])( std::string target ) const
+		= { &Intern::makeShrubberyCreationForm,
+			&Intern::makeRobotomyRequestForm,
+			&Intern::makePresidentialPardonForm };
+	std::string		formNames[3]
+		= { "shrubbery creation",
+			"robotomy request",
+			"presidential pardon" };
+	Form*			result;
+
+	try
+	{
+		int		i;
+		for ( i = 0; i < 3; i++ )
+		{
+			if ( formNames[i] == formName )
+			{
+				result = (this->*formCalls[i])( formTarget );
+				break ;
+			}
+		}
+		if ( i == 3 )
+			throw CouldntFindFormException();
+	}
+	catch ( std::exception& e )
+	{
+		std::cout << e.what() << std::endl;
+		return ( NULL );
+	}
+
+	return ( result );
+}
+
+Form*		Intern::makeShrubberyCreationForm( std::string target ) const
+{
+	std::cout << "Intern creates ShrubberyCreationForm" << std::endl;
+
+	return ( new ShrubberyCreationForm( target ) );
+}
+
+Form*		Intern::makeRobotomyRequestForm( std::string target ) const
+{
+	std::cout << "Intern creates RobotomyRequestForm" << std::endl;
+
+	return ( new RobotomyRequestForm( target ) );
+}
+
+Form*		Intern::makePresidentialPardonForm( std::string target ) const
+{
+	std::cout << "Intern creates PresidentialPardonForm" << std::endl;
+
+	return ( new PresidentialPardonForm( target ) );
+}
+
+const char*	Intern::CouldntFindFormException::what() const throw()
+{
+	return ( "Error: Couldn't find a form associated with this name" );
 }
--- day_05/ex03/Intern.class.hpp
+++ day_05/ex03/Intern.class.hpp
@@ -6,15 +6,25 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:57:06 by mvidal-a          #+#    #+#             */
-/*   Updated: 2021/10/23 17:13:55 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2021/10/24 16:01:22 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __INTERN_CLASS_HPP
 # define __INTERN_CLASS_HPP
 
+# include "ShrubberyCreationForm.class.hpp"
+# include "RobotomyRequestForm.class.hpp"
+# include "PresidentialPardonForm.class.hpp"
+
 class	Intern
 {
+	private:
+
+		Form*	makeShrubberyCreationForm( std::string target ) const;
+		Form*	makeRobotomyRequestForm( std::string target ) const;
+		Form*	makePresidentialPardonForm( std::string target ) const;
+
 	public:
 
 		Intern( void );
@@ -23,7 +33,13 @@ class	Intern
 
 		Intern&		operator=( const Intern& rhs );
 
-		Form*	makeForms( std::string formName, std::string formTarget );
+		Form*	makeForm( std::string formName, std::string formTarget );
+
+		class	CouldntFindFormException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
 };
 
 #endif // __INTERN_CLASS_HPP
