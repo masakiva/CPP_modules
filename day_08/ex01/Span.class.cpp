c7d0f8c8f991573a87599a45d8931fd64020553e
pêle-mêle <kumo.git@mailnull.com>
2021-10-31 23:34:25 +0100

    ex01 wip

--- day_08/ex01/Span.class.cpp
+++ day_08/ex01/Span.class.cpp
@@ -6,38 +6,59 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 17:07:59 by mvidal-a          #+#    #+#             */
-/*   Updated: 2021/10/30 17:29:57 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2021/10/31 23:32:42 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.class.hpp"
 
-Span::Span( void )
-{
-}
+Span::Span( void ) {}
 
 Span::Span( unsigned int N )
 {
+	this->_N = N;
 }
 
-Span::Span( const Span& src )
-{
-	*this = src;
-}
+Span::Span( const Span& src ) { *this = src; }
 
-Span::~Span( void )
-{
-}
+Span::~Span( void ) { }
 
 Span&	Span::operator=( const Span& rhs )
 {
 	if ( this == &rhs )
 		return ( *this );
 
+	this->_nbs = rhs._nbs;
+	this->_N = rhs._N;
 
 	return ( *this );
 }
 
+int&	Span::operator[]( unsigned int i )
+{
+	return ( this->_nbs[i] );
+}
+
 void	Span::addNumber( int new_nb )
 {
+	if ( _nbs.size() < this->_N )
+		this->_nbs.push_back( new_nb );
+	else
+		throw VectorFullException();
+}
+
+unsigned int	Span::shortestSpan( void )
+{
+	return ( 1 );
+}
+
+unsigned int	Span::longestSpan( void )
+{
+	return ( *std::max_element( _nbs.begin(), _nbs.end() )
+			- *std::min_element( _nbs.begin(), _nbs.end() ) );
+}
+
+const char*		Span::VectorFullException::what() const throw()
+{
+	return ( "Error: no more room for new numbers" );
 }
--- day_08/ex01/Span.class.hpp
+++ day_08/ex01/Span.class.hpp
@@ -6,31 +6,42 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 16:59:46 by mvidal-a          #+#    #+#             */
-/*   Updated: 2021/10/30 17:29:46 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2021/10/31 23:23:30 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SPAN_CLASS_HPP__
 # define __SPAN_CLASS_HPP__
 
-#include <cstddef> // size_t
+#include <vector>
+#include <algorithm>
 
 class	Span
 {
 	private:
 
-		std::list<int>			_nb_lst;
+		std::vector<int>	_nbs;
+		unsigned int		_N;
+
+		Span( void );
 
 	public:
 
-		Span( void );
 		Span( unsigned int N );
 		Span( const Span& src );
 		~Span( void );
 
 		Span&	operator=( const Span& rhs );
+		int&	operator[]( unsigned int i );
 
-		void	addNumber( int new_nb );
+		void			addNumber( int new_nb );
+		unsigned int	shortestSpan( void );
+		unsigned int	longestSpan( void );
+
+		class	VectorFullException
+		{
+			virtual const char* what() const throw();
+		};
 
 };
 
